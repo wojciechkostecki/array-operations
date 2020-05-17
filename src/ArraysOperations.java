5f13db3deadcea9f738ca2846d04ba505b6311d6
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-05-17 22:32:12 +0200

    Creating a method that returns arrays with more characters

--- src/ArraysOperations.java
+++ src/ArraysOperations.java
@@ -19,5 +19,21 @@ public class ArraysOperations {
         return sum;
     }
 
-    
+    public String[] returnArrayWithMoreCharacters(String[] array1, String[] array2){
+        int arrayLength1 = countNumbersChar(array1);
+        int arrayLength2 = countNumbersChar(array2);
+        if (arrayLength1>arrayLength2){
+            return array1;
+        } else {
+            return array2;
+        }
+    }
+
+    private int countNumbersChar(String[] array){
+        int length = 0;
+        for (int i = 0; i < array.length; i++) {
+            length += array[i].toCharArray().length;
+        }
+        return length;
+    }
 }
