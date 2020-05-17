e1494918aef4052490f93a036748076b5582ef27
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-05-17 22:58:01 +0200

    Creating a method that returns the largest one-dimensional array from a two-dimensional array

--- src/ArraysOperations.java
+++ src/ArraysOperations.java
@@ -46,4 +46,42 @@ public class ArraysOperations {
     public int findSmallestNumber(int [] array){
         return Arrays.stream(array).min().getAsInt();
     }
+
+    public int[] return1dArray(int[][] array2d){
+        int biggestArray = 0;
+        int currentArray = 0;
+        int index = 0;
+        for (int i = 0; i < array2d.length; i++) {
+
+            for (int j = 0; j < array2d[i].length; j++) {
+                currentArray = currentArray + array2d[i][j];
+            }
+            if (currentArray > biggestArray) {
+                biggestArray = currentArray;
+                index = i;
+            }
+        }
+        return array2d[index];
+    }
+
+    public void printInt2dArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printStringArray(String[] array) {
+        for (String s : array) {
+            System.out.print(s + " ");
+        }
+    }
+
+    public void printIntArray(int[] array){
+        for (int i : array) {
+            System.out.println(i + " ");
+        }
+    }
 }
